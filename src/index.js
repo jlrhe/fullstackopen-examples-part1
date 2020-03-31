bb0cbb22fa87ce9962938a3aa47929b911aecb02
jlrhe <jan@heikel.fi>
2020-03-31 20:49:20 +0300

    part 1c up to event handlers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = ({ description, name, age }) => { //made sure the order doesn't matter when destructuring
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  console.log(name)
+  console.log(description)
+  console.log(age)
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    if(typeof age == "number") {
+      return yearNow - age
+    }
+    return '"redacted"'
+  }
+  
+  return (
+    <div>
+      <p>Hello {name}. You are {description}. I think you may have been born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const CounterSeconds = (props) => {
+  const [counter, incrementCounter] = useState(0)
+  const [counterOn, toggleCounterOn] = useState(true)
+
+  if(counterOn) {
+    setTimeout( () => incrementCounter(counter + 1), 1000)
+  }
+  
+  const stopStartCounter = () => {
+    toggleCounterOn(!counterOn)
+  }
+
+  return (
+  <>
+  <div>You have been on this page {counter} seconds.</div>
+  <button onClick={stopStartCounter}>Start/Stop counter</button>
+  </>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const wife = "beautiful"
+  
+  return (
+    <div>
+      <Hello name="Janne" description="a genius" age={38} />
+      <Hello name="Sampo" description="the best" age={3} />
+      <Hello name="Minna" description={wife.toUpperCase()} age="secret" />
+      <p>It's {now.toDateString()}</p>
+      <CounterSeconds />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
